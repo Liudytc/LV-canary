89a6d115083f942720f2ebdeeeefbf59df995168
Thomas Gooch <code4days@Thomass-MacBook-Pro.local>
2020-12-09 14:41:06 -0500

    implemented changes for multiple regions

--- example.js
+++ example.js
@@ -13,32 +13,14 @@ const logger = winston.createLogger({
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
-    //
-    // The simple format outputs
-    // `${level}: ${message} ${[Object with everything else]}`
-    //
     format.simple()
-    //
-    // Alternatively you could use this custom printf format if you
-    // want to control where the timestamp comes in your final message.
-    // Try replacing `format.simple()` above with this:
-    //
-    // format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
     new winston.transports.File({ filename: '/Users/code4days/Documents/logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: '/Users/code4days/Documents/logs/combined.log' }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.simple(),
@@ -49,35 +31,55 @@ logger.info("starting application.");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36');
-  const urls = ['https://us.louisvuitton.com/eng-us/products/nano-speedy-monogram-010575',
-    'https://uk.louisvuitton.com/eng-us/products/nano-speedy-monogram-010575'];
-  // const url = 'https://us.louisvuitton.com/eng-us/products/trunk-clutch-epi-nvprod1040054v#M53052'
-  logger.info("urls: " + url);
-  await page.goto(url, { waitUntil: 'networkidle2' });
-  // await page.screenshot({path: 'example.png'});
-  const found = await page.evaluate(() => window.find("In stock"));
-  logger.info("in stock: " + found);
-  await browser.close();
-  // if found is true... reach out to sns topic and send text message.
-  if (found) {
-    logger.info("The bag is available");
-    // Create publish parameters
-    var params = {
-      Message: 'Nano Speedy now available on US website!!!', /* required */
-      TopicArn: 'arn:aws:sns:us-east-1:233567662909:LV-Availability-Topic'
-    };
-    logger.info("Params for SNS created.");
-    var publishTextPromise = new AWS.SNS().publish(params).promise();
+  // const urls = ['https://us.louisvuitton.com/eng-us/products/nano-speedy-monogram-010575',
+  //   'https://uk.louisvuitton.com/eng-gb/products/nano-speedy-monogram-010575'];
+  const urls = ['https://us.louisvuitton.com/eng-us/products/trunk-clutch-epi-nvprod1040054v#M53052',
+    'https://uk.louisvuitton.com/eng-gb/products/trunk-clutch-epi-nvprod1040054v#M53052']
 
-    // Handle promise's fulfilled/rejected states
-    publishTextPromise.then(
-      function (data) {
-        logger.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
-        logger.info("MessageID is " + data.MessageId);
-      }).catch(
-        function (err) {
-          logger.error(err, err.stack);
-        });
-  }
+  logger.info("urls: " + urls);
+
+  urls.forEach(url => {
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    // await page.screenshot({path: 'example.png'});
+    location = url.slice(8, 10);
+    logger.info('location: ', location);
+    switch (location) {
+      case 'us':
+        const found = await page.evaluate(() => window.find("In stock"));
+        break;
+      case 'uk':
+        const found = await page.evaluate(() => window.find("Available"));
+        break;
+      default:
+        logger.info("location UNKOWN!");
+        break;
+    }
+    const found = await page.evaluate(() => window.find("In stock"));
+    logger.info("in stock: " + found);
+    await browser.close();
+    // if found is true... reach out to sns topic and send text message.
+    if (found) {
+      logger.info("The bag is available");
+      // Create publish parameters
+      var params = {
+        Message: 'Eagle has LANDED!!!\n ', url, /* required */
+        TopicArn: 'arn:aws:sns:us-east-1:233567662909:LV-Availability-Topic'
+      };
+      logger.info("Params for SNS created.");
+      var publishTextPromise = new AWS.SNS().publish(params).promise();
+
+      // Handle promise's fulfilled/rejected states
+      publishTextPromise.then(
+        function (data) {
+          logger.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+          logger.info("MessageID is " + data.MessageId);
+        }).catch(
+          function (err) {
+            logger.error(err, err.stack);
+          });
+    }
+  });
+
+
   logger.info("application ended.");
 })();
