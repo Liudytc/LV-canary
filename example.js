3b3ede753979dc570bc0be482e0d8773358554b5
Thomas Gooch <code4days@Thomass-MacBook-Pro.local>
2020-12-09 15:22:26 -0500

    added log message after search

--- example.js
+++ example.js
@@ -9,7 +9,6 @@ AWS.config.update({ region: 'us-east-1' });
 
 const logger = winston.createLogger({
   format: format.combine(
-    format.label({ label: '[my-label]' }),
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
@@ -80,42 +79,41 @@ logger.info("starting application.");
           logger.error(err, err.stack);
         });
   }
-  
+  logger.info("Finished searching US website");
 })();
-(async () => {
-  logger.info("Searching UK website");
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36');
-  // const url = 'https://uk.louisvuitton.com/eng-gb/products/nano-speedy-monogram-010575';
-  const url = 'https://uk.louisvuitton.com/eng-gb/products/trunk-clutch-epi-nvprod1040054v#M53052'
-  logger.info("url: " + url);
-  await page.goto(url, { waitUntil: 'networkidle2' });
-  // await page.screenshot({path: 'example.png'});
-  const found = await page.evaluate(() => window.find("Available"));
-  logger.info("Available: " + found);
-  await browser.close();
-  // if found is true... reach out to sns topic and send text message.
-  if (found) {
-    logger.info("The bag is available");
-    // Create publish parameters
-    var params = {
-      Message: 'Nano Speedy now available on UK website!!!', /* required */
-      TopicArn: 'arn:aws:sns:us-east-1:233567662909:LV-Availability-Topic'
-    };
-    logger.info("Params for SNS created.");
-    var publishTextPromise = new AWS.SNS().publish(params).promise();
+// (async () => {
+//   logger.info("Searching UK website");
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36');
+//   const url = 'https://uk.louisvuitton.com/eng-gb/products/nano-speedy-monogram-010575';
+//   // const url = 'https://uk.louisvuitton.com/eng-gb/products/trunk-clutch-epi-nvprod1040054v#M53052'
+//   logger.info("url: " + url);
+//   await page.goto(url, { waitUntil: 'networkidle2' });
+//   // await page.screenshot({path: 'example.png'});
+//   const found = await page.evaluate(() => window.find("Available"));
+//   logger.info("Available: " + found);
+//   await browser.close();
+//   // if found is true... reach out to sns topic and send text message.
+//   if (found) {
+//     logger.info("The bag is available");
+//     // Create publish parameters
+//     var params = {
+//       Message: 'Nano Speedy now available on UK website!!!', /* required */
+//       TopicArn: 'arn:aws:sns:us-east-1:233567662909:LV-Availability-Topic'
+//     };
+//     logger.info("Params for SNS created.");
+//     var publishTextPromise = new AWS.SNS().publish(params).promise();
 
-    // Handle promise's fulfilled/rejected states
-    publishTextPromise.then(
-      function (data) {
-        logger.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
-        logger.info("MessageID is " + data.MessageId);
-      }).catch(
-        function (err) {
-          logger.error(err, err.stack);
-        });
-  }
-  
-})();
-logger.info('application ended');
+//     // Handle promise's fulfilled/rejected states
+//     publishTextPromise.then(
+//       function (data) {
+//         logger.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+//         logger.info("MessageID is " + data.MessageId);
+//       }).catch(
+//         function (err) {
+//           logger.error(err, err.stack);
+//         });
+//   }
+// logger.info("Finished searching UK website");
+// })();
